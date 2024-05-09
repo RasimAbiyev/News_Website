d40f36564e55bca1510dc8a6b99b15e5090c4f1d
RasimAbiyev <abiyev.rasim@gmail.com>
2024-05-09 12:29:10 +0400

    Change

--- newsproject/newsapp/views.py
+++ newsproject/newsapp/views.py
@@ -4,7 +4,7 @@ from newsapi import NewsApiClient
 def index(request): 
       
     newsapi = NewsApiClient(api_key ='d0d74ee48df24d3dbb25dba9c418ae53') 
-    top = newsapi.get_top_headlines(sources ='techcrunch') 
+    top = newsapi.get_top_headlines(sources ='techcrunch')
   
     l = top['articles'] 
     desc =[] 
